fa7f758f6aed74f86cbddaa1403f6dca6cf7915d
CharlesCoeder <croederdev@gmail.com>
2024-10-15 02:33:42 -0400

    Hide caret

--- packages/config/src/tamagui.config.ts
+++ packages/config/src/tamagui.config.ts
@@ -87,6 +87,23 @@ export const config = createTamagui({
     hoverNone: { hover: 'none' },
     pointerCoarse: { pointer: 'coarse' },
   }),
+  defaultProps: {
+    Text: {
+      style: {
+        caretColor: 'transparent',
+      },
+    },
+    Paragraph: {
+      style: {
+        caretColor: 'transparent',
+      },
+    },
+    H1: {
+      style: {
+        caretColor: 'transparent',
+      },
+    },
+  },
 })
 
 // for the compiler to find it
