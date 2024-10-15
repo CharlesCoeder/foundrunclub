4d824dacd89c621c538b11f81db55916a7e869a1
CharlesCoeder <croederdev@gmail.com>
2024-10-15 16:16:48 -0400

    Update metadata

--- apps/next/app/layout.tsx
+++ apps/next/app/layout.tsx
@@ -2,18 +2,17 @@ import type { Metadata } from 'next'
 import { NextTamaguiProvider } from './NextTamaguiProvider'
 
 export const metadata: Metadata = {
-  title: 'Tamagui • App Router',
-  description: 'Tamagui, Solito, Expo & Next.js',
-  icons: '/favicon.ico',
+  title: 'FOUND Run Club',
+  description:
+    'Join fellow FOUND residents for group runs, track your progress, and stay fit together. Your student housing running community awaits!',
+  icons: '/favicon.ico', // Gym shoes icons created by Arslan Haider - Flaticon
 }
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     // You can use `suppressHydrationWarning` to avoid the warning about mismatched content during hydration in dev mode
-    <html
-      lang="en"
-      suppressHydrationWarning
-    >
+    <html lang="en" suppressHydrationWarning>
       <body>
         <NextTamaguiProvider>{children}</NextTamaguiProvider>
       </body>
